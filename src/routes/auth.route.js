21b2f9801f8b8469e53291ac8041e7d938ccbe3d
IndrajitSir <indrajitmandal779@gmail.com>
2025-03-09 22:03:16 +0530

    Route for user Authentication

---
+++ src/routes/auth.route.js
@@ -0,0 +1,6 @@
+import {Router} from 'express';
+import { register, login } from '../controllers/auth.controller';
+const router = Router();
+
+router.route("/register").post(register);
+router.route("/login").post(login);

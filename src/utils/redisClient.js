df07ae34539f9bf6d1fe220c41713142d9290411
INDRAJIT MANDAL <149069491+IndrajitSir@users.noreply.github.com>
2025-09-01 02:24:43 +0530

    Update redisClient.js

--- src/utils/redisClient.js
+++ src/utils/redisClient.js
@@ -1,7 +1,7 @@
 import Redis from 'ioredis';
 import logger from './Logger/logger.js'
 
-const redis = new Redis();
+const redis = new Redis(process.env.REDIS_URL);
 
 redis.on("connect", () => {
     logger.info("!!Connected to Redis!!")
